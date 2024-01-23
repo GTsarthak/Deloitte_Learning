690da415cda4d509d193e86f714dde26d2bbc846
Gupta <sarthakgupta29@deloitte.com>
2024-01-23 12:39:13 +0530

    commit2

--- note.py
+++ note.py
@@ -1,6 +1,6 @@
 print("My name is Sarthak Gupta")
-
-print("I am a part of Deloitte")
+print("a+b")
+print("I am a part of Deloitte Bangalore")
 
 print("Hey, This is Saritha trying out the pull request")
 print("hiiii")
